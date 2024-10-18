7254f830968c3c50354c9907308e04b8c09f8ae4
Julian Manns <julian.manns@five-d.ai>
2024-10-18 11:12:37 +0200

    Modify vcpkg for radarsuite

--- ports/usd/portfile.cmake
+++ ports/usd/portfile.cmake
@@ -1,44 +1,80 @@
 # Don't file if the bin folder exists. We need exe and custom files.
 set(VCPKG_POLICY_EMPTY_PACKAGE enabled)
 
-message(STATUS [=[
-The usd port does not work with the version of Threading Building Blocks (tbb) currently chosen by vcpkg's baselines,
-and does not expect to be updated to work with current versions soon. See
-https://github.com/PixarAnimationStudios/USD/issues/1600
-
-If you must use this port in your project, pin a version of tbb of 2020_U3 or older via a manifest file.
-See https://vcpkg.io/en/docs/examples/versioning.getting-started.html for instructions.
-]=])
-
 string(REGEX REPLACE "^([0-9]+)[.]([0-9])\$" "\\1.0\\2" USD_VERSION "${VERSION}")
 
 vcpkg_from_github(
     OUT_SOURCE_PATH SOURCE_PATH
     REPO PixarAnimationStudios/OpenUSD
     REF "v${USD_VERSION}"
-    SHA512 e510f6421caba5e74c6efe5b56b17e9c9741ece0cfd5020148ca89b3ac32bd8781ab00dfc7a134163c85af3f4f01f2529a9baa5a9df9b0c80cbca003e6d199e2
+    SHA512 7d4404980579c4de3c155386184ca9d2eb96756ef6e090611bae7b4c21ad942c649f73a39b74ad84d0151ce6b9236c4b6c0c555e8e36fdd86304079e1c2e5cbe
     HEAD_REF master
     PATCHES
         fix_build-location.patch
 )
 
-if(NOT VCPKG_TARGET_IS_WINDOWS)
-file(REMOVE ${SOURCE_PATH}/cmake/modules/FindTBB.cmake)
+vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
+    FEATURES
+        monolithic          VCPKG_FEATURE_MONOLITHIC
+        imaging             VCPKG_FEATURE_IMAGING
+        commandline-tools   VCPKG_FEATURE_COMMANDLINE_TOOLS
+        embree-support      VCPKG_FEATURE_EMBREE_SUPPORT
+)
+
+if (VCPKG_FEATURE_MONOLITHIC)
+    set(USD_BUILD_MONOLITHIC ON)
+else()
+    set(USD_BUILD_MONOLITHIC OFF)
+endif()
+
+if (VCPKG_FEATURE_IMAGING)
+    set(USD_BUILD_IMAGING ON)
+    # install pip and then pyside6
+    execute_process(
+        COMMAND "${CURRENT_INSTALLED_DIR}/tools/python3/python3" -m ensurepip
+        RESULT_VARIABLE ensurepip_result
+    )
+    if (NOT ensurepip_result EQUAL 0)
+        message(FATAL_ERROR "Failed to install pip")
+    endif()
+    execute_process(
+        COMMAND "${CURRENT_INSTALLED_DIR}/tools/python3/python3" -m pip install --upgrade pip
+        RESULT_VARIABLE pyside6_result
+    )
+    execute_process(
+        COMMAND "${CURRENT_INSTALLED_DIR}/tools/python3/python3" -m pip install PySide6 PyOpenGL jinja2
+        RESULT_VARIABLE pyside6_result
+    )
+else()
+    set(USD_BUILD_IMAGING OFF)
+endif()
+
+if (VCPKG_FEATURE_COMMANDLINE_TOOLS)
+    set(USD_BUILD_TOOLS ON)
+else()
+    set(USD_BUILD_TOOLS OFF)
+endif()
+
+if(VCPKG_FEATURE_EMBREE_SUPPORT)
+    set(USD_BUILD_EMBREE_PLUGIN ON)
+else()
+    set(USD_BUILD_EMBREE_PLUGIN OFF)
 endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
     OPTIONS
         -DPXR_BUILD_ALEMBIC_PLUGIN:BOOL=OFF
-        -DPXR_BUILD_EMBREE_PLUGIN:BOOL=OFF
-        -DPXR_BUILD_IMAGING:BOOL=OFF
-        -DPXR_BUILD_MONOLITHIC:BOOL=OFF
+        -DPXR_BUILD_EMBREE_PLUGIN:BOOL=${USD_BUILD_EMBREE_PLUGIN}
+        -DPXR_BUILD_IMAGING:BOOL=${USD_BUILD_IMAGING}
+        -DPXR_BUILD_MONOLITHIC:BOOL=${USD_BUILD_MONOLITHIC}
         -DPXR_BUILD_TESTS:BOOL=OFF
-        -DPXR_BUILD_USD_IMAGING:BOOL=OFF
-        -DPXR_ENABLE_PYTHON_SUPPORT:BOOL=OFF
+        -DPXR_BUILD_USD_IMAGING:BOOL=${USD_BUILD_IMAGING}
+        -DPXR_ENABLE_PYTHON_SUPPORT:BOOL=${USD_BUILD_IMAGING}
         -DPXR_BUILD_EXAMPLES:BOOL=OFF
         -DPXR_BUILD_TUTORIALS:BOOL=OFF
-        -DPXR_BUILD_USD_TOOLS:BOOL=OFF
+        -DPXR_BUILD_USD_TOOLS:BOOL=${USD_BUILD_TOOLS}
+        -DPXR_ENABLE_GL_SUPPORT:BOOL=ON
 )
 
 vcpkg_cmake_install()
@@ -46,6 +82,7 @@ vcpkg_cmake_install()
 # The CMake files installation is not standard in USD and will install pxrConfig.cmake in the prefix root and
 # pxrTargets.cmake in "cmake" so we are moving pxrConfig.cmake in the same folder and patch the path to pxrTargets.cmake
 vcpkg_replace_string(${CURRENT_PACKAGES_DIR}/pxrConfig.cmake "/cmake/pxrTargets.cmake" "/pxrTargets.cmake")
+vcpkg_replace_string(${CURRENT_PACKAGES_DIR}/pxrConfig.cmake "${PXR_CMAKE_DIR}/include" "${VCPKG_IMPORT_PREFIX}/include")
 
 file(
     RENAME
