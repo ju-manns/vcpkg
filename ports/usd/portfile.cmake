25008cb644db798a8e3e84cb498938aa3168d88f
Julian Manns <julian.manns@five-d.ai>
2024-10-23 14:49:13 +0200

    Fix usd portfile for windows

--- ports/usd/portfile.cmake
+++ ports/usd/portfile.cmake
@@ -18,54 +18,65 @@ vcpkg_check_features(OUT_FEATURE_OPTIONS FEATURE_OPTIONS
         monolithic          VCPKG_FEATURE_MONOLITHIC
         imaging             VCPKG_FEATURE_IMAGING
         commandline-tools   VCPKG_FEATURE_COMMANDLINE_TOOLS
-        embree-support      VCPKG_FEATURE_EMBREE_SUPPORT
+        usdview             VCPKG_FEATURE_USDVIEW
 )
 
-if (VCPKG_FEATURE_MONOLITHIC)
+if(VCPKG_FEATURE_MONOLITHIC)
     set(USD_BUILD_MONOLITHIC ON)
 else()
     set(USD_BUILD_MONOLITHIC OFF)
 endif()
 
-if (VCPKG_FEATURE_IMAGING)
+if(VCPKG_FEATURE_USDVIEW)
+    set(USD_BUILD_USDVIEW ON)
+else()
+    set(USD_BUILD_USDVIEW OFF)
+endif()
+
+if(VCPKG_FEATURE_IMAGING)
     set(USD_BUILD_IMAGING ON)
-    # install pip and then pyside6
+    # install pip and then python packages
+    if(VCPKG_TARGET_IS_WINDOWS)
+        set(PYTHON_BIN "${CURRENT_INSTALLED_DIR}/tools/python3/python.exe")
+    else()
+        set(PYTHON_BIN "${CURRENT_INSTALLED_DIR}/tools/python3/python3")
+    endif()
     execute_process(
-        COMMAND "${CURRENT_INSTALLED_DIR}/tools/python3/python3" -m ensurepip
+        COMMAND ${PYTHON_BIN} -m ensurepip
         RESULT_VARIABLE ensurepip_result
     )
-    if (NOT ensurepip_result EQUAL 0)
+    if(NOT ensurepip_result EQUAL 0)
         message(FATAL_ERROR "Failed to install pip")
     endif()
     execute_process(
-        COMMAND "${CURRENT_INSTALLED_DIR}/tools/python3/python3" -m pip install --upgrade pip
-        RESULT_VARIABLE pyside6_result
+        COMMAND ${PYTHON_BIN} -m pip install --upgrade pip
+        RESULT_VARIABLE pip_result
     )
     execute_process(
-        COMMAND "${CURRENT_INSTALLED_DIR}/tools/python3/python3" -m pip install PySide6 PyOpenGL jinja2
-        RESULT_VARIABLE pyside6_result
+        COMMAND ${PYTHON_BIN} -m pip install jinja2
+        RESULT_VARIABLE pip_result
     )
+    if(USD_BUILD_USDVIEW)
+        execute_process(
+            COMMAND ${PYTHON_BIN} -m pip install pyside6 opengl
+            RESULT_VARIABLE pip_result
+        )
+    endif()
 else()
     set(USD_BUILD_IMAGING OFF)
 endif()
 
-if (VCPKG_FEATURE_COMMANDLINE_TOOLS)
+if(VCPKG_FEATURE_COMMANDLINE_TOOLS)
     set(USD_BUILD_TOOLS ON)
 else()
     set(USD_BUILD_TOOLS OFF)
-endif()
-
-if(VCPKG_FEATURE_EMBREE_SUPPORT)
-    set(USD_BUILD_EMBREE_PLUGIN ON)
-else()
-    set(USD_BUILD_EMBREE_PLUGIN OFF)
 endif()
 
 vcpkg_cmake_configure(
     SOURCE_PATH ${SOURCE_PATH}
     OPTIONS
         -DPXR_BUILD_ALEMBIC_PLUGIN:BOOL=OFF
-        -DPXR_BUILD_EMBREE_PLUGIN:BOOL=${USD_BUILD_EMBREE_PLUGIN}
+        -DPXR_BUILD_EMBREE_PLUGIN:BOOL=OFF
         -DPXR_BUILD_IMAGING:BOOL=${USD_BUILD_IMAGING}
         -DPXR_BUILD_MONOLITHIC:BOOL=${USD_BUILD_MONOLITHIC}
         -DPXR_BUILD_TESTS:BOOL=OFF
@@ -75,6 +86,8 @@ vcpkg_cmake_configure(
         -DPXR_BUILD_TUTORIALS:BOOL=OFF
         -DPXR_BUILD_USD_TOOLS:BOOL=${USD_BUILD_TOOLS}
         -DPXR_ENABLE_GL_SUPPORT:BOOL=ON
+        -DPXR_ENABLE_PRECOMPILED_HEADERS:BOOL=OFF
+        -DPXR_BUILD_USDVIEW:BOOL=${USD_BUILD_USDVIEW}
 )
 
 vcpkg_cmake_install()
@@ -118,8 +131,13 @@ if(VCPKG_TARGET_IS_WINDOWS)
     endfunction()
 
     # fix dll path for cmake
-    file_replace_regex(${CURRENT_PACKAGES_DIR}/share/pxr/pxrTargets-debug.cmake "debug/lib/([a-zA-Z0-9_]+)\\.dll" "debug/bin/\\1.dll")
-    file_replace_regex(${CURRENT_PACKAGES_DIR}/share/pxr/pxrTargets-release.cmake "lib/([a-zA-Z0-9_]+)\\.dll" "bin/\\1.dll")
+
+    if(NOT VCPKG_BUILD_TYPE STREQUAL "release")
+        file_replace_regex(${CURRENT_PACKAGES_DIR}/share/pxr/pxrTargets-debug.cmake "debug/lib/([a-zA-Z0-9_]+)\\.dll" "debug/bin/\\1.dll")
+    endif()
+    if(NOT VCPKG_BUILD_TYPE STREQUAL "debug")
+        file_replace_regex(${CURRENT_PACKAGES_DIR}/share/pxr/pxrTargets-release.cmake "lib/([a-zA-Z0-9_]+)\\.dll" "bin/\\1.dll")
+    endif()
 
     # fix plugInfo.json for runtime
     file(GLOB_RECURSE PLUGINFO_FILES ${CURRENT_PACKAGES_DIR}/lib/usd/*/resources/plugInfo.json)
